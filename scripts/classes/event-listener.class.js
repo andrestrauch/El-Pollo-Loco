425e225b3393d784c08aedb82d89143f6615744e
Andre Strauch <andrestrauch@gmx.de>
2026-08-31 21:02:41 +0200

    addet mobil button

--- scripts/classes/event-listener.class.js
+++ scripts/classes/event-listener.class.js
@@ -1,47 +1,79 @@
 import { Keyboard } from "./keyboard.class.js";
 
 export class EventListener {
-    static addEventListener() {
-        document.addEventListener("keydown", function (event) {
-            if (event.code == "Space") {
-                Keyboard.SPACE = true;
-            }
-            if (event.code == "ArrowUp") {
-                Keyboard.UP = true;
-            }
-            if (event.code == "ArrowDown") {
-                Keyboard.DOWN = true;
-            }
-            if (event.code == "ArrowLeft") {
-                Keyboard.LEFT = true;
-            }
-            if (event.code == "ArrowRight") {
-                Keyboard.RIGHT = true;
-            }
-            if (event.code == "KeyD") {
-                Keyboard.D = true;
-            }
-        });
+	static addEventListener() {
+		document.addEventListener("keydown", function (event) {
+			if (event.code == "Space") {
+				Keyboard.SPACE = true;
+			}
+			if (event.code == "ArrowUp") {
+				Keyboard.UP = true;
+			}
+			if (event.code == "ArrowDown") {
+				Keyboard.DOWN = true;
+			}
+			if (event.code == "ArrowLeft") {
+				Keyboard.LEFT = true;
+			}
+			if (event.code == "ArrowRight") {
+				Keyboard.RIGHT = true;
+			}
+			if (event.code == "KeyD") {
+				Keyboard.D = true;
+			}
+		});
 
-        document.addEventListener("keyup", function (event) {
-            if (event.code == "Space") {
-                Keyboard.SPACE = false;
-            }
-            if (event.code == "ArrowUp") {
-                Keyboard.UP = false;
-            }
-            if (event.code == "ArrowDown") {
-                Keyboard.DOWN = false;
-            }
-            if (event.code == "ArrowLeft") {
-                Keyboard.LEFT = false;
-            }
-            if (event.code == "ArrowRight") {
-                Keyboard.RIGHT = false;
-            }
-            if (event.code == "KeyD") {
-                Keyboard.D = false;
-            }
-        });
-    }
+		document.addEventListener("keyup", function (event) {
+			if (event.code == "Space") {
+				Keyboard.SPACE = false;
+			}
+			if (event.code == "ArrowUp") {
+				Keyboard.UP = false;
+			}
+			if (event.code == "ArrowDown") {
+				Keyboard.DOWN = false;
+			}
+			if (event.code == "ArrowLeft") {
+				Keyboard.LEFT = false;
+			}
+			if (event.code == "ArrowRight") {
+				Keyboard.RIGHT = false;
+			}
+			if (event.code == "KeyD") {
+				Keyboard.D = false;
+			}
+		});
+
+		document.getElementById(`btnLeft`).addEventListener(`mousedown`, () => {
+			Keyboard.LEFT = true;
+		});
+
+		document.getElementById(`btnLeft`).addEventListener(`mouseup`, () => {
+			Keyboard.LEFT = false;
+		});
+
+		document.getElementById(`btnRight`).addEventListener(`mousedown`, () => {
+			Keyboard.RIGHT = true;
+		});
+
+		document.getElementById(`btnRight`).addEventListener(`mouseup`, () => {
+			Keyboard.RIGHT = false;
+		});
+
+		document.getElementById(`btnUp`).addEventListener(`mousedown`, () => {
+			Keyboard.UP = true;
+		});
+
+		document.getElementById(`btnUp`).addEventListener(`mouseup`, () => {
+			Keyboard.UP = false;
+		});
+
+		document.getElementById(`btnThrow`).addEventListener(`mousedown`, () => {
+			Keyboard.D = true;
+		});
+
+		document.getElementById(`btnThrow`).addEventListener(`mouseup`, () => {
+			Keyboard.D = false;
+		});
+	}
 }
--- scripts/script.js
+++ scripts/script.js
@@ -8,16 +8,17 @@ import { World } from "./classes/world.class.js";
 export function init() {
 	document.getElementById("startBtn").classList.add("d_none");
 	// IntervalHub.stopAllIntervals();
-
+	const canvas = document.getElementById("canvas");
+	let lvStart = -2;
+	let lvEnd = 7;
+	Globals.cvsW = canvas.width;
+	Globals.cvsH = canvas.height;
+	Globals.lvStart = canvas.width * lvStart;
+	Globals.lvEnd = canvas.width * lvEnd;
 	setTimeout(() => {
-		const canvas = document.getElementById("canvas");
-		let lvStart = -2;
-		let lvEnd = 7;
-		Globals.cvsW = canvas.width;
-		Globals.cvsH = canvas.height;
-		Globals.lvStart = canvas.width * lvStart;
-		Globals.lvEnd = canvas.width * lvEnd;
 		Globals.level1 = new Level(enemies, clouds, coins, bottles, lvStart, lvEnd, Globals.cvsW);
+	}, 100);
+	setTimeout(() => {
 		Globals.world = new World(canvas);
 		EventListener.addEventListener();
 	}, 250);
