d2ea945dd97cd7f9ff03c448e464934cffbeb5d9
Andre Strauch <andrestrauch@gmx.de>
2026-09-01 00:33:01 +0200

    changed level content loading

--- scripts/level/level1.js
+++ scripts/level/level1.js
@@ -4,130 +4,142 @@ import { Endboss } from "../classes/endboss.class.js";
 import { MiniChicken } from "../classes/chicken-mini.class.js";
 import { Collectables } from "../classes/collectables.class.js";
 
-export const enemies = [
-	new Chicken(),
-	new Chicken(),
-	new Chicken(),
-	new Chicken(),
-	new Chicken(),
-	new Chicken(),
-	new Chicken(),
-	new Chicken(),
-	new Chicken(),
-	new Chicken(),
-	new Chicken(),
-	new Chicken(),
-	new MiniChicken(),
-	new MiniChicken(),
-	new MiniChicken(),
-	new MiniChicken(),
-	new MiniChicken(),
-	new Endboss(),
-];
+export class Level1 {
+	static enemies;
+	static clouds;
+	static coins;
+	static bottles;
 
-export const clouds = [
-	new Cloud(-2000, 1 + Math.random() * 2),
-	new Cloud(-1000, 1 + Math.random() * 2),
-	new Cloud(0, 1 + Math.random() * 2),
-	new Cloud(1000, 1 + Math.random() * 2),
-	new Cloud(2000, 1 + Math.random() * 2),
-	new Cloud(3000, 1 + Math.random() * 2),
-	new Cloud(4000, 1 + Math.random() * 2),
-	new Cloud(5000, 1 + Math.random() * 2),
-	new Cloud(6000, 1 + Math.random() * 2),
-	new Cloud(7000, 1 + Math.random() * 2),
-	new Cloud(8000, 1 + Math.random() * 2),
-];
+	static addContent() {
+		Level1.clearArrays();
+		Level1.addEnemy();
+		Level1.addClouds();
+		Level1.addCoins();
+		Level1.addBottle();
+	}
 
-export const coins = [
-	new Collectables(300, 610, "coin"),
-	new Collectables(400, 610, "coin"),
-	new Collectables(500, 610, "coin"),
-	new Collectables(700, 610, "coin"),
-	new Collectables(900, 610, "coin"),
-	new Collectables(1100, 610, "coin"),
-	new Collectables(1500, 610, "coin"),
-	new Collectables(1800, 610, "coin"),
-	new Collectables(2000, 610, "coin"),
-	new Collectables(2400, 610, "coin"),
+	static clearArrays() {
+		Level1.enemies = [];
+		Level1.clouds = [];
+		Level1.coins = [];
+		Level1.bottles = [];
+	}
 
-	new Collectables(300, 500, "coin"),
-	new Collectables(400, 500, "coin"),
-	new Collectables(500, 500, "coin"),
-	new Collectables(700, 500, "coin"),
-	new Collectables(900, 500, "coin"),
-	new Collectables(1100, 500, "coin"),
-	new Collectables(1500, 500, "coin"),
-	new Collectables(1800, 500, "coin"),
-	new Collectables(2000, 500, "coin"),
-	new Collectables(2400, 500, "coin"),
+	static addEnemy() {
+		for (let i = 0; i <= 12; i++) {
+			Level1.enemies.push(new Chicken());
+		}
 
-	new Collectables(300, 390, "coin"),
-	new Collectables(400, 390, "coin"),
-	new Collectables(500, 390, "coin"),
-	new Collectables(700, 390, "coin"),
-	new Collectables(900, 390, "coin"),
-	new Collectables(1100, 390, "coin"),
-	new Collectables(1500, 390, "coin"),
-	new Collectables(1800, 390, "coin"),
-	new Collectables(2000, 390, "coin"),
-	new Collectables(2400, 390, "coin"),
+		for (let i = 0; i <= 5; i++) {
+			Level1.enemies.push(new MiniChicken());
+		}
+		Level1.enemies.push(new Endboss());
+	}
 
-	new Collectables(300, 280, "coin"),
-	new Collectables(400, 280, "coin"),
-	new Collectables(500, 280, "coin"),
-	new Collectables(700, 280, "coin"),
-	new Collectables(900, 280, "coin"),
-	new Collectables(1100, 280, "coin"),
-	new Collectables(1500, 280, "coin"),
-	new Collectables(1800, 280, "coin"),
-	new Collectables(2000, 280, "coin"),
-	new Collectables(2400, 280, "coin"),
+	static addClouds() {
+		Level1.clouds.push(new Cloud(-2000, 1 + Math.random() * 2));
+		Level1.clouds.push(new Cloud(-1000, 1 + Math.random() * 2));
+		Level1.clouds.push(new Cloud(0, 1 + Math.random() * 2));
+		Level1.clouds.push(new Cloud(1000, 1 + Math.random() * 2));
+		Level1.clouds.push(new Cloud(2000, 1 + Math.random() * 2));
+		Level1.clouds.push(new Cloud(3000, 1 + Math.random() * 2));
+		Level1.clouds.push(new Cloud(4000, 1 + Math.random() * 2));
+		Level1.clouds.push(new Cloud(5000, 1 + Math.random() * 2));
+		Level1.clouds.push(new Cloud(6000, 1 + Math.random() * 2));
+		Level1.clouds.push(new Cloud(7000, 1 + Math.random() * 2));
+		Level1.clouds.push(new Cloud(8000, 1 + Math.random() * 2));
+	}
 
-	new Collectables(2800, 610, "coin"),
-	new Collectables(2800, 500, "coin"),
-	new Collectables(2800, 390, "coin"),
-	new Collectables(2800, 280, "coin"),
+	static addCoins() {
+		Level1.coins.push(new Collectables(300, 610, "coin"));
+		Level1.coins.push(new Collectables(400, 610, "coin"));
+		Level1.coins.push(new Collectables(500, 610, "coin"));
+		Level1.coins.push(new Collectables(700, 610, "coin"));
+		Level1.coins.push(new Collectables(900, 610, "coin"));
+		Level1.coins.push(new Collectables(1100, 610, "coin"));
+		Level1.coins.push(new Collectables(1500, 610, "coin"));
+		Level1.coins.push(new Collectables(1800, 610, "coin"));
+		Level1.coins.push(new Collectables(2000, 610, "coin"));
+		Level1.coins.push(new Collectables(2400, 610, "coin"));
 
-	new Collectables(3200, 610, "coin"),
-	new Collectables(3200, 500, "coin"),
-	new Collectables(3200, 390, "coin"),
-	new Collectables(3200, 280, "coin"),
+		Level1.coins.push(new Collectables(300, 500, "coin"));
+		Level1.coins.push(new Collectables(400, 500, "coin"));
+		Level1.coins.push(new Collectables(500, 500, "coin"));
+		Level1.coins.push(new Collectables(700, 500, "coin"));
+		Level1.coins.push(new Collectables(900, 500, "coin"));
+		Level1.coins.push(new Collectables(1100, 500, "coin"));
+		Level1.coins.push(new Collectables(1500, 500, "coin"));
+		Level1.coins.push(new Collectables(1800, 500, "coin"));
+		Level1.coins.push(new Collectables(2000, 500, "coin"));
+		Level1.coins.push(new Collectables(2400, 500, "coin"));
 
-	new Collectables(3500, 610, "coin"),
-	new Collectables(3500, 500, "coin"),
-	new Collectables(3500, 390, "coin"),
-	new Collectables(3500, 280, "coin"),
+		Level1.coins.push(new Collectables(300, 390, "coin"));
+		Level1.coins.push(new Collectables(400, 390, "coin"));
+		Level1.coins.push(new Collectables(500, 390, "coin"));
+		Level1.coins.push(new Collectables(700, 390, "coin"));
+		Level1.coins.push(new Collectables(900, 390, "coin"));
+		Level1.coins.push(new Collectables(1100, 390, "coin"));
+		Level1.coins.push(new Collectables(1500, 390, "coin"));
+		Level1.coins.push(new Collectables(1800, 390, "coin"));
+		Level1.coins.push(new Collectables(2000, 390, "coin"));
+		Level1.coins.push(new Collectables(2400, 390, "coin"));
 
-	new Collectables(3800, 610, "coin"),
-	new Collectables(3800, 500, "coin"),
-	new Collectables(3800, 390, "coin"),
-	new Collectables(3800, 280, "coin"),
+		Level1.coins.push(new Collectables(300, 280, "coin"));
+		Level1.coins.push(new Collectables(400, 280, "coin"));
+		Level1.coins.push(new Collectables(500, 280, "coin"));
+		Level1.coins.push(new Collectables(700, 280, "coin"));
+		Level1.coins.push(new Collectables(900, 280, "coin"));
+		Level1.coins.push(new Collectables(1100, 280, "coin"));
+		Level1.coins.push(new Collectables(1500, 280, "coin"));
+		Level1.coins.push(new Collectables(1800, 280, "coin"));
+		Level1.coins.push(new Collectables(2000, 280, "coin"));
+		Level1.coins.push(new Collectables(2400, 280, "coin"));
 
-	new Collectables(-500, 610, "coin"),
-	new Collectables(-500, 500, "coin"),
-	new Collectables(-500, 390, "coin"),
-	new Collectables(-500, 280, "coin"),
+		Level1.coins.push(new Collectables(2800, 610, "coin"));
+		Level1.coins.push(new Collectables(2800, 500, "coin"));
+		Level1.coins.push(new Collectables(2800, 390, "coin"));
+		Level1.coins.push(new Collectables(2800, 280, "coin"));
 
-	new Collectables(-1000, 610, "coin"),
-	new Collectables(-1000, 500, "coin"),
-	new Collectables(-1000, 390, "coin"),
-	new Collectables(-1000, 280, "coin"),
+		Level1.coins.push(new Collectables(3200, 610, "coin"));
+		Level1.coins.push(new Collectables(3200, 500, "coin"));
+		Level1.coins.push(new Collectables(3200, 390, "coin"));
+		Level1.coins.push(new Collectables(3200, 280, "coin"));
 
-	new Collectables(-2000, 610, "coin"),
-	new Collectables(-2000, 500, "coin"),
-	new Collectables(-2000, 390, "coin"),
-	new Collectables(-2000, 280, "coin"),
-];
+		Level1.coins.push(new Collectables(3500, 610, "coin"));
+		Level1.coins.push(new Collectables(3500, 500, "coin"));
+		Level1.coins.push(new Collectables(3500, 390, "coin"));
+		Level1.coins.push(new Collectables(3500, 280, "coin"));
 
-export const bottles = [
-	new Collectables(-200, 600, "bottle"),
-	new Collectables(-1050, 600, "bottle"),
-	new Collectables(-2050, 600, "bottle"),
-	new Collectables(200, 600, "bottle"),
-	new Collectables(1050, 600, "bottle"),
-	new Collectables(1950, 600, "bottle"),
-	new Collectables(2350, 600, "bottle"),
-	new Collectables(3050, 600, "bottle"),
-	new Collectables(3700, 600, "bottle"),
-];
+		Level1.coins.push(new Collectables(3800, 610, "coin"));
+		Level1.coins.push(new Collectables(3800, 500, "coin"));
+		Level1.coins.push(new Collectables(3800, 390, "coin"));
+		Level1.coins.push(new Collectables(3800, 280, "coin"));
+
+		Level1.coins.push(new Collectables(-500, 610, "coin"));
+		Level1.coins.push(new Collectables(-500, 500, "coin"));
+		Level1.coins.push(new Collectables(-500, 390, "coin"));
+		Level1.coins.push(new Collectables(-500, 280, "coin"));
+
+		Level1.coins.push(new Collectables(-1000, 610, "coin"));
+		Level1.coins.push(new Collectables(-1000, 500, "coin"));
+		Level1.coins.push(new Collectables(-1000, 390, "coin"));
+		Level1.coins.push(new Collectables(-1000, 280, "coin"));
+
+		Level1.coins.push(new Collectables(-2000, 610, "coin"));
+		Level1.coins.push(new Collectables(-2000, 500, "coin"));
+		Level1.coins.push(new Collectables(-2000, 390, "coin"));
+		Level1.coins.push(new Collectables(-2000, 280, "coin"));
+	}
+
+	static addBottle() {
+		Level1.bottles.push(new Collectables(-200, 600, "bottle"));
+		Level1.bottles.push(new Collectables(-1050, 600, "bottle"));
+		Level1.bottles.push(new Collectables(-2050, 600, "bottle"));
+		Level1.bottles.push(new Collectables(200, 600, "bottle"));
+		Level1.bottles.push(new Collectables(1050, 600, "bottle"));
+		Level1.bottles.push(new Collectables(1950, 600, "bottle"));
+		Level1.bottles.push(new Collectables(2350, 600, "bottle"));
+		Level1.bottles.push(new Collectables(3050, 600, "bottle"));
+		Level1.bottles.push(new Collectables(3700, 600, "bottle"));
+	}
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,10 @@
-import { enemies, clouds, coins, bottles } from "./level/level1.js";
+// import { enemies, clouds, coins, bottles } from "./level/level1.js";
 import { EventListener } from "./classes/event-listener.class.js";
 import { Globals } from "./classes/globals.class.js";
 import { IntervalHub } from "./classes/interval-hub.class.js";
 import { Level } from "./classes/level.class.js";
 import { World } from "./classes/world.class.js";
+import { Level1 } from "./level/level1.js";
 
 export function init() {
 	document.getElementById("startBtn").classList.add("d_none");
@@ -15,8 +16,10 @@ export function init() {
 	Globals.cvsH = canvas.height;
 	Globals.lvStart = canvas.width * lvStart;
 	Globals.lvEnd = canvas.width * lvEnd;
+
+	Level1.addContent();
 	setTimeout(() => {
-		Globals.level1 = new Level(enemies, clouds, coins, bottles, lvStart, lvEnd, Globals.cvsW);
+		Globals.level1 = new Level(Level1.enemies, Level1.clouds, Level1.coins, Level1.bottles, lvStart, lvEnd, Globals.cvsW);
 	}, 100);
 	setTimeout(() => {
 		Globals.world = new World(canvas);
@@ -24,24 +27,23 @@ export function init() {
 	}, 250);
 }
 
-function showFullscreen() {
-	let fullscreen = document.getElementById("fullscreen");
-	fullscreen.requestFullscreen();
-}
-
 export function gameRestart() {
 	document.getElementById(`gameOver`).classList.remove(`d-flex`);
 	document.getElementById(`gameOver`).classList.add(`d_none`);
-
 	document.getElementById(`gameEnd`).classList.remove(`d-flex`);
 	document.getElementById(`gameEnd`).classList.add(`d_none`);
 
-	Globals.isDead = false;
 	Globals.isHurt = false;
+	Globals.isDead = false;
 	Globals.bossDead = false;
 	IntervalHub.stopAllIntervals();
 
 	setTimeout(() => {
 		init();
-	}, 250);
+	}, 500);
+}
+
+function showFullscreen() {
+	let fullscreen = document.getElementById("fullscreen");
+	fullscreen.requestFullscreen();
 }
